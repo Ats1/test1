91d33b985ba066e2deaa8bfef9d9ef1c1a5d64d4
Amit <amit1212b@gmail.com>
2022-09-11 14:38:22 +0530

    first code push in git

---
+++ mongotest.py
@@ -0,0 +1,15 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://ats:amit818@cluster0.tupsxnu.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d= {
+    'name':'att',
+    'email': 'att@gmail.com',
+    'surname':'shfgt'
+}
+db1= client['mongodb1']
+
+coll= db1['test1']
+coll.insert_one(d )
