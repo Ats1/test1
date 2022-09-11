84870bb8f6fadbc013b27711ed05a39f11e6b3ee
Amit <amit1212b@gmail.com>
2022-09-11 14:44:15 +0530

    first code push in git

--- mongotest.py
+++ mongotest.py
@@ -1,6 +1,6 @@
 import pymongo
 
-client = pymongo.MongoClient("mongodb+srv://ats:amit818@cluster0.tupsxnu.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://ats:ats11@cluster0.tupsxnu.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 print(db)
 
